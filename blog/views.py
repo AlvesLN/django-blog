f631b61099b65585cfb0f36d3dd9a57726ae0847
Kauan Alves <kauanalvessilva557@gmail.com>
2023-06-28 11:00:31 -0300

    28/06

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 #Define uma function viem chamada index.
 def index(request):
     #return HttpResponse('Olá Django - index')
-    return render(request, 'index.html') 
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'}) 
 
 # Define uma function view chamada ola.
 def ola(request):
