5ae81551dbcf1b0c0570ddd64d349efa2339248b
AlvesLN <kauanalvessilva557@gmail.com>
2023-09-06 10:35:08 -0300

    Aula 30/08

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from django.views.generic.detail import DetailView
 
-# Create your views here
+# Create your views here.
 
-# Inclui a classe HttpResponse.
 from django.http import HttpResponse
+from blog.models import Post # Acrescentar
 
-#Define uma function viem chamada index.
 def index(request):
-    #return HttpResponse('Olá Django - index')
-    return render(request, 'index.html', {'titulo': 'Últimos Artigos'}) 
+    # return HttpResponse('Olá Django - index')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
 
-# Define uma function view chamada ola.
-def ola(request):
-    #return HttpResponse('Olá Django')
-    return render(request, 'home.html')
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
--- webapp/settings.py
+++ webapp/settings.py
@@ -1,4 +1,5 @@
-import os
+import os # Acrescentar no topo do arquivo, na primeira linha
+
 """
 Django settings for webapp project.
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7y&uwnio1*#s5#fq*hm14lso@#sv0@_09ws7=h*0#z_shzv!9'
+SECRET_KEY = 'django-insecure-vmsta2e#eu#v2)v^p#(1o+4&a9dz)%7gg6ecbp7rll5=@-b9a2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'django_extensions', # Acrescentar
 ]
 
 MIDDLEWARE = [
@@ -77,11 +79,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,5 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Inclua no final do arquivo as duas configurações abaixo
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
